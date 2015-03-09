5ce00e8b2e885223c835b50454c70cd2d2e6205f
wenh7788 <wenh7788@163.com>
2015-03-09 16:23:30 +0800

    wenhui_这个可能是一个不稳定的版本

--- src/main/java/com/ai/gzesp/controller/WeShopController.java
+++ src/main/java/com/ai/gzesp/controller/WeShopController.java
@@ -1,5 +1,8 @@
 package com.ai.gzesp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +10,14 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.ai.gzesp.service.WeShopService;
 
+/**
+ * 一级功能：wap端<br> 
+ * 二级功能：店铺主页
+ *
+ * @author xmh
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
 @Controller
 @RequestMapping("/weShop")
 public class WeShopController {
@@ -14,14 +25,28 @@ public class WeShopController {
     @Autowired
     private WeShopService weShopService;
     
+    /**
+     * 三级页面：店铺主页 <br>
+     * 〈功能详细描述〉
+     *
+     * @return
+     * @see [相关类/方法](可选)
+     * @since [产品/模块版本](可选)
+     */
     @RequestMapping("/index")
     public ModelAndView index(){
         ModelAndView mav = new ModelAndView("weShopIndex.ftl");
         //从数据库获取信息赋值
+        //广告轮播图片的url
+        List<String> banners = new ArrayList<String>();
+        banners.add("banner_iphone6.png");
+        banners.add("banner_honer_x1.png");
+        mav.addObject("banners", banners);
+        
+        //店铺名称手机微信
         mav.addObject("name", "喻露露");
         mav.addObject("phone", "18685292522"); 
         mav.addObject("weixin", "1306520198@qq.com"); 
         return mav;
     }
-
 }

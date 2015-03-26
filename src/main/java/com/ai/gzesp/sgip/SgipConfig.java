4d4a34dc745626e307664375847adee33b814a9d
ximinhui <xmh@ximinhui>
2015-03-26 20:27:15 +0800

    20150326 ximh 短信接口，resVersion自动注入

--- src/main/java/com/ai/gzesp/sgip/SgipConfig.java
+++ src/main/java/com/ai/gzesp/sgip/SgipConfig.java
@@ -14,15 +14,15 @@ public class SgipConfig {
     
     public static String host = "58.16.249.70"; // 主机名，网关IP
     public static int port = 8801; // 端口号，这里特别注意下，接入协议中写的是8804，害得我调了很久，后来才知道改了，所以，这个在接入前，建议与网关人员确定
-    public static String SPNumber = "10655717";//接入号码
+    public static String SPNumber = "10655211";//接入号码改为 10655211 原接入号 10655717 不对
     public static String ChargeNumber = "000000000000000000000"; // 计费号码，我们是白名单
     public static String ServiceType = "ESP";//业务代码，由SP定义，字符
     public static String CorpId = "73112"; // 企业代码
     public static String login_Name = "aiced"; // 登陆名
     public static String login_PassWord = "aiced1234"; // 登陆密码
-    public static String source_addr = "3085173112"; 
+    public static String source_addr = "3085173112";  //"3085173112"; 
     public static int transaction_timeout = 10; 
-    public static int read_timeout = 15;
+    public static int read_timeout = 60;
     public static String debug =  "true";
     public static int version = 1;
     

acb80394908f676bcf8caee450b466923c7477ae
wenh7788 <wenh7788@163.com>
2015-05-06 11:53:42 +0800

    文辉 短信测试

--- src/main/java/com/ai/gzesp/utils/SmsUtils.java
+++ src/main/java/com/ai/gzesp/utils/SmsUtils.java
@@ -159,7 +159,7 @@ public class SmsUtils {
 			url_con.setConnectTimeout(SmsUtils.connectTimeOut);
 			url_con.setReadTimeout(SmsUtils.readTimeOut);
 			url_con.setDoOutput(true);
-			url_con.setRequestProperty("Content-Type", "plain/text; charset=utf-8"); 
+			//url_con.setRequestProperty("Content-Type", "plain/text; charset=utf-8"); 
 			//url_con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
 			byte[] b = params.toString().getBytes();
 			url_con.getOutputStream().write(b, 0, b.length);

648d216a4386482e93ed3130ac0c5ca8aa9344f0
wenh7788 <wenh7788@163.com>
2015-06-15 16:40:41 +0800

    文辉 修改冲突文件

--- src/main/webapp/resources/js/acctSecurity.js
+++ src/main/webapp/resources/js/acctSecurity.js
@@ -14,17 +14,7 @@ function rowClick(){
 	var user_id=document.getElementById("hide_user_id");
 	
 	if(subTitle.innerHTML == "设置"){
-<<<<<<< HEAD
 		window.location.href="../payPasswordSet/"+user_id.value;
-=======
-		alert('点击了设置');
-//    	window.location.href='../acctSecurity/'+$("#hide_user_id").val();
-    	window.location.href='../acctSecurity/'+'2';
-
-
-	}else if(subTitle.innerHTML == "重置" ){
-		alert('点击了重置');
->>>>>>> branch 'master' of https://github.com/aiced/gzesp.git
 
 	}else if(subTitle.innerHTML == "重置" ){
 		window.location.href="../payPasswordResetVerify/"+user_id.value;
